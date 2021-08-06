88dcb07b902d74dcec789818a1e1e67f02ef7a20
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-08-05 20:46:42 -0500

    💌 Configured meta tags

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,14 @@ export default function Home() {
 
         <link rel="stylesheet" href="/style.css" />
         <link rel="stylesheet" href="/blog.css" />
-        <meta name="image" content="/Site Demo.png"></meta>
+        <meta property="og:title" content="Chetachi's Portfolio"></meta>
+        <meta
+          property="og:description"
+          content="Chetachi's personal portfolio site"
+        ></meta>
+        <meta property="og:image" content="/sitedemo.png"></meta>
+        <meta property="og:url" content="https://chetachilabs.com"></meta>
+        <meta name="twitter:card" content="/sitedemo.png"></meta>
         <meta
           name="keywords"
           content="HTML, CSS, JavaScript, React, NextJS, Software, Software Developer, Software Engineer, Tech, Junior Developer, Junior Developer Texas, Software Developer Texas, Software Engineer Texas, Junior Developer Houston, Junior Developer Austin, Software Developer Austin, Software Engineer Austin, Junior Developer Dallas, Software Developer Dallas, Software Engineer Dallas"
