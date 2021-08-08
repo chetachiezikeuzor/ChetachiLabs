8902603235261a51b0ae1db0632ee582be752391
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-08-08 17:55:22 -0500

    👩🏽‍💻 Revised pdf

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,6 @@ export default function Home() {
       themeButton.addEventListener("click", () => {
         document.body.classList.toggle(darkTheme);
         document.querySelector("img").classList.toggle(darkTheme);
-        document.querySelector("iframe").classList.toggle(darkTheme);
         document.querySelector("html").classList.toggle(darkTheme);
 
         themeButton.classList.toggle(iconTheme);
@@ -211,7 +210,7 @@ export default function Home() {
                 <a
                   target="_blank"
                   className="nav-link"
-                  href="/Chetachi Ezikeuzor Resume.pdf"
+                  href="/ChetachiEzikeuzorResume.pdf"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -1092,7 +1091,6 @@ export default function Home() {
                     width: "100%",
                     height: "0",
                     paddingTop: "56.2500%",
-                    paddingBottom: "48px",
                     boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
                     overflow: "hidden",
                     borderRadius: "0px",
@@ -1218,6 +1216,28 @@ export default function Home() {
                   clipRule="evenodd"
                   d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974c0 4.406 2.857 8.145 6.821 9.465c.499.09.679-.217.679-.481c0-.237-.008-.865-.011-1.696c-2.775.602-3.361-1.338-3.361-1.338c-.452-1.152-1.107-1.459-1.107-1.459c-.905-.619.069-.605.069-.605c1.002.07 1.527 1.028 1.527 1.028c.89 1.524 2.336 1.084 2.902.829c.091-.645.351-1.085.635-1.334c-2.214-.251-4.542-1.107-4.542-4.93c0-1.087.389-1.979 1.024-2.675c-.101-.253-.446-1.268.099-2.64c0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336a9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021c.545 1.372.203 2.387.099 2.64c.64.696 1.024 1.587 1.024 2.675c0 3.833-2.33 4.675-4.552 4.922c.355.308.675.916.675 1.846c0 1.334-.012 2.41-.012 2.737c0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974C22 6.465 17.535 2 12.026 2z"
                   fill=""
+                />
+              </svg>
+            </a>
+
+            <a
+              href="/ChetachiEzikeuzorResume.pdf"
+              className="social-link-footer link-icon-center"
+              target="_blank"
+            >
+              <svg
+                className="footer-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="img"
+                width="1em"
+                height="1em"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M20 6h-3V4c0-1.103-.897-2-2-2H9c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v11c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2zm-5-2v2H9V4h6zM4 8h16v4h-3v-2h-2v2H9v-2H7v2H4V8zm0 11v-5h3v2h2v-2h6v2h2v-2h3.001v5H4z"
+                  fill="currentColor"
                 />
               </svg>
             </a>
